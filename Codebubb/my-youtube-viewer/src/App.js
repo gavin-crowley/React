2e914f7ddcbca77772b7fdace4c0b5ff6354b861
Gavin Crowley <42537387+gavin-crowley@users.noreply.github.com>
2022-07-27 15:53:17 +0100

    creating the search form

--- Codebubb/my-youtube-viewer/src/App.js
+++ Codebubb/my-youtube-viewer/src/App.js
@@ -5,23 +5,30 @@ import { Video } from './Video';
 export function App() {
     const baseUrl = 'https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fwww.youtube.com%2Ffeeds%2Fvideos.xml%3Fchannel_id%3D'
     const [videos, setVideos] = useState([])
-    const [currentChannelId, setCurrentChannelId] = useState('UCuvTObpdx__W966kbdIZ4_Q')
+    const [currentChannelId, setCurrentChannelId] = useState('')
+    const [channelId, setChannelId] = useState('')
 
     useEffect(() => {
         (async () => {
-            const data = await fetch(`${baseUrl}${currentChannelId}`)
-            .then(response => response.json())
-           
-            setVideos(data.items)
-            console.log(data.items);
-            console.log(videos);
+            if (currentChannelId) {
+                
+                const data = await fetch(`${baseUrl}${currentChannelId}`)
+                .then(response => response.json())
+                
+                setVideos(data.items)
+                console.log(data.items);
+                console.log(videos);
+            }
         })()
     }, [currentChannelId])
 
     return (
         <div className="app-container">
             <h1>Latest YouTube Videos</h1>
-
+            <div className="search">
+                <input type="text" onChange={event => setChannelId(event.target.value)} placeholder='Enter your favorite channel ID' />
+                <button onClick={() => setCurrentChannelId(channelId)}>Get Videos</button>
+            </div>
             {videos.map(video => (
                 <div key={video.guid} className="videos__items">
                     <div className="video__image">
