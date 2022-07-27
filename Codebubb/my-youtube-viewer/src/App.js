8d0c844b4d979e8da6cdb5cc8350a49849cdaeb1
Gavin Crowley <42537387+gavin-crowley@users.noreply.github.com>
2022-07-27 16:33:44 +0100

    fixed css, app working in one file

--- Codebubb/my-youtube-viewer/src/App.js
+++ Codebubb/my-youtube-viewer/src/App.js
@@ -48,22 +48,25 @@ export function App() {
       </div>
       <div className="search__errors">{searchError}</div>
       {channelName && <h2>Videos from {channelName}</h2>}
-      {videos.map((video) => (
-        <div key={video.guid} className="videos__items">
-          <div className="video__image">
-            <a href={video.link} target="_blank" rel="noopener noreferrer">
-              <img
-                src={`https://i4.ytimg.com/vi/${
-                  video.guid.split(":")[2]
-                }/mqdefault.jpg`}
-              />
-            </a>
-          </div>
-          <div className="video__footer">
-            <p>{video.title}</p>
-          </div>
-        </div>
-      ))}
+
+      <div className="videos">
+          {videos.map((video) => (
+            <div key={video.guid} className="videos__item">
+              <div className="video__image">
+                <a href={video.link} target="_blank" rel="noopener noreferrer">
+                  <img
+                    src={`https://i4.ytimg.com/vi/${
+                      video.guid.split(":")[2]
+                    }/mqdefault.jpg`}
+                  />
+                </a>
+              </div>
+              <div className="video__footer">
+                <p>{video.title}</p>
+              </div>
+            </div>
+          ))}
+      </div>
     </div>
   );
 }
