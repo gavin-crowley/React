ca6fa412ff6b6893311b1bea747a7f3074b33e10
Gavin Crowley <42537387+gavin-crowley@users.noreply.github.com>
2022-07-27 15:22:58 +0100

    templating the video data

--- Codebubb/my-youtube-viewer/src/App.js
+++ Codebubb/my-youtube-viewer/src/App.js
@@ -14,12 +14,26 @@ export function App() {
            
             setVideos(data.items)
             console.log(data.items);
+            console.log(videos);
         })()
     }, [currentChannelId])
 
     return (
         <div className="app-container">
             <h1>Latest YouTube Videos</h1>
+
+            {videos.map(video => (
+                <div key={video.guid} className="videos__items">
+                    <div className="video__image">
+                        <a href={video.link} target="_blank" rel="noopener noreferrer">
+                            <img src={`https://i4.ytimg.com/vi/${video.guid.split(':')[2]}/mqdefault.jpg`} />
+                        </a>
+                    </div>
+                    <div className="video__footer">
+                        <p>{video.title}</p>
+                    </div>
+                </div>
+            ))}
         </div>
     );
 };
