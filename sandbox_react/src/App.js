72f862a9c14bfce7b7b8688493f398642169d1ce
Gavin Crowley <42537387+gavin-crowley@users.noreply.github.com>
2022-09-23 17:03:59 +0100

    equinox relearn

--- sandbox_react/src/App.js
+++ sandbox_react/src/App.js
@@ -1,35 +1,53 @@
-import React, { useState } from 'react';
-import JSONDATA from './db.json';
+import { React, useState } from 'react';
 
 import './styles.css';
 
 export default function App() {
-  const [searchTerm, setSearchTerm] = useState('');
+  const [events, setEvents] = useState([
+    { title: "mario's birthday bash", id: 1 },
+    { title: "bowser's live stream", id: 2 },
+    { title: 'race on moo moo farm', id: 3 },
+  ]);
+
+  const [showEvents, setShowEvents] = useState(true);
+
+  const handleDelete = (id) => {
+    setEvents((prevEvents) => {
+      return prevEvents.filter((event) => {
+        return event.id !== id;
+      });
+    });
+  };
+
   return (
-    <div className='App'>
-      <input
-        type='text'
-        placeholder='Search here...'
-        onChange={(event) => {
-          setSearchTerm(event.target.value);
-        }}
-        value={searchTerm}
-      />
-      <div>
-        {JSONDATA.persons
-          .filter((item) => {
-            if (searchTerm === '') {
-              return item;
-            } else if (
-              item.name.toLowerCase().includes(searchTerm.toLowerCase())
-            ) {
-              return item;
-            }
-          })
-          .map((item) => (
-            <p key={item.id}>{item.name}</p>
-          ))}
-      </div>
+    <div className='app'>
+      {showEvents && <div>
+        <button
+          onClick={() => {
+            setShowEvents(false);
+          }}
+        >
+          Hide Events
+        </button>
+      </div>}
+      {!showEvents && <div>
+        <button
+          onClick={() => {
+            setShowEvents(true);
+          }}
+        >
+          Show Events
+        </button>
+      </div>}
+      {showEvents &&
+        events.map((event, i) => (
+          <div key={event.id}>
+            <h2>
+              {i} - {event.title}
+            </h2>
+            <button onClick={() => handleDelete(event.id)}>delete event</button>
+          </div>
+        ))}
     </div>
   );
 }
