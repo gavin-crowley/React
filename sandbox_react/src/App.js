a96921a45402f2b366853f046be4edb433ab5852
Gavin Crowley <42537387+gavin-crowley@users.noreply.github.com>
2022-09-24 14:11:47 +0100

    sep 24 redo

--- sandbox_react/src/App.js
+++ sandbox_react/src/App.js
@@ -1,53 +1,54 @@
-import { React, useState } from 'react';
+import './App.css'
+import React, { useState } from 'react'
+import Title from './components/Title'
+import Modal from './components/Modal'
+import EventList from './components/EventList'
+import NewEventForm from './components/NewEventForm'
 
-import './styles.css';
+function App() {
+  const [showModal, setShowModal] = useState(false)
+  const [showEvents, setShowEvents] = useState(true)
+  const [events, setEvents] = useState([])
 
-export default function App() {
-  const [events, setEvents] = useState([
-    { title: "mario's birthday bash", id: 1 },
-    { title: "bowser's live stream", id: 2 },
-    { title: 'race on moo moo farm', id: 3 },
-  ]);
+  const handleClick = (id) => {
+    setEvents(prevEvents => {
+      return prevEvents.filter(event => id !== event.id)
+    })
+  }
 
-  const [showEvents, setShowEvents] = useState(true);
+  const handleClose = () => {
+    setShowModal(false)
+  }
 
-  const handleDelete = (id) => {
-    setEvents((prevEvents) => {
-      return prevEvents.filter((event) => {
-        return event.id !== id;
-      });
-    });
-  };
+  const subtitle = "All the latest events in Marioland"
 
   return (
-    <div className='app'>
-      {showEvents && <div>
-        <button
-          onClick={() => {
-            setShowEvents(false);
-          }}
-        >
-          Hide Events
-        </button>
-      </div>}
-      {!showEvents && <div>
-        <button
-          onClick={() => {
-            setShowEvents(true);
-          }}
-        >
-          Show Events
-        </button>
-      </div>}
-      {showEvents &&
-        events.map((event, i) => (
-          <div key={event.id}>
-            <h2>
-              {i} - {event.title}
-            </h2>
-            <button onClick={() => handleDelete(event.id)}>delete event</button>
-          </div>
-        ))}
+    <div className="App">
+      <Title title="Marioland Events" subtitle={subtitle} />
+      
+      {showEvents && (
+        <div>
+          <button onClick={() => setShowEvents(false)}>Hide Events</button>
+        </div>
+      )}
+      {!showEvents && (
+        <div>
+          <button onClick={() => setShowEvents(true)}>Show Events</button>
+        </div>
+      )}
+      {showEvents && <EventList events={events} handleClick={handleClick} />}
+      
+      {showModal && (
+        <Modal handleClose={handleClose}>
+          <NewEventForm events={events} setEvents={setEvents}/>
+        </Modal>
+      )}
+
+      <div>
+        <button onClick={() => setShowModal(true)}>Add New Event</button>
+      </div>
     </div>
   );
 }
+
+export default App;
