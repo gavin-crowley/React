3b3173b3bfc126e39eeb57c04760040acb865012
Gavin Crowley <42537387+gavin-crowley@users.noreply.github.com>
2023-01-08 16:31:44 +0000

    TS Basics

---
+++ TypeScript/fcc-ts-tut/TS-Basics.tsx
@@ -0,0 +1,106 @@
+import React from 'react';
+import './App.css';
+
+let name: string;
+// let name: any;
+// let name: unknown; // better than any
+let age: number | string;
+let isStudent: boolean;
+let hobbies: string[];
+let role: [number, string];
+
+// function printName(name: string) {
+//   console.log(name)
+// }
+
+let printName: (name: string) => void; // void returns undefined
+// let printName: (name: string) => never; // nothing returned
+
+// printName("Ia")
+
+// type Person = {
+//   name: string;
+//   age?: number;
+// }
+
+// let person: Person = {
+//   name: "Saoirse",
+// }
+
+// let lotsOfPeople: Person[];
+
+
+// instead of type can use interface
+// type Person = {
+//   name: string;
+//   age?: number;
+// }
+
+interface Person {
+  name: string;
+  age?: number;
+}
+
+// --------------
+type X = {
+  a: string;
+  b: number;
+}
+
+type Y = X & {
+  c: string;
+  d: number;
+}
+
+// let y: Y = {
+//   c: "abc",
+//   d: 42,
+//   a: "def",
+//   b: 45
+// }
+
+// can also use interface
+
+// interface Guy {
+//   profession: string;
+// }
+
+interface Guy extends Person {
+  profession: string;
+}
+
+// let gavin: Guy = {
+//   profession: "Programmer",
+//   name: "Crowley",
+//   age: 43
+// }
+
+// ------------------
+
+// can also
+
+// type X = Person &{
+//   a: string;
+//   b: number;
+// }
+
+
+// ------------------
+
+// also
+
+interface Person extends X {
+  name: string;
+  age?: number;
+} 
+
+
+function App() {
+  return (
+    <div className="App">
+      Hey
+    </div>
+  );
+}
+
+export default App;
